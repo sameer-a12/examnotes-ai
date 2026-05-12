7399550e12476896f8d1d55635e682181456a4eb
sameer-a12 <sameerali06567@gmail.com>
2026-05-12 13:40:37 +0530

    add current user route and authentication controller

---
+++ 1.ExamNotesAI/server/routes/user.route.js
@@ -0,0 +1,11 @@
+import express from "express"
+import isAuth from "../middlewares/isAuth.js"
+import { getCurrentUser } from "../controllers/user.controller.js"
+
+
+const userRouter = express.Router()
+
+
+userRouter.get("/currentuser",isAuth,getCurrentUser)
+
+export default userRouter
