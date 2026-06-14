ae80a583ae1e91b15db0f5c40c2a8d7bbc446cbf
sameer-a12 <sameerali06567@gmail.com>
2026-06-14 17:35:30 +0530

    fix: correct RechartSetUp import path typo

--- 1.ExamNotesAI/client/src/components/FinalResult.jsx
+++ 1.ExamNotesAI/client/src/components/FinalResult.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import MermaidSetup from './MermaidSetup';
-import RechartSetUp from './RechartSetUp';
+import RechartSetUp from './RechartSetup';
 import { downloadPdf } from '../services/api';
 const markDownComponent = {
     h1: ({ children }) => (
