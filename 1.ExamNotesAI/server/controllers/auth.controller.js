19fec4c99653dc5ff146781ad25942caf91af76c
sameer-a12 <sameerali06567@gmail.com>
2026-06-14 18:15:48 +0530

    fix: correct sameSite cookie option

--- 1.ExamNotesAI/server/controllers/auth.controller.js
+++ 1.ExamNotesAI/server/controllers/auth.controller.js
@@ -13,13 +13,12 @@ export const googleAuth = async (req,res) => {
             })
         }
         let token = await getToken(user._id)
-        res.cookie("token" , token , {
-            httpOnly:true,
-            secure:true,
-            samesite:"none",
-            maxAge:7 * 24 * 60 * 60 * 1000
-
-        })
+        res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
         return res.status(200).json(user)
     } catch (error) {
         return res.status(500).json({message:`googleSignup Error  ${error}`})
